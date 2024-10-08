98d930fa088a6c0b38d396213a29786154a15cb2
AlessioMartella7 <alessio.mart87@gmail.com>
2024-10-08 17:21:42 +0200

    added axios call and printed poster on page

--- js/script.js
+++ js/script.js
@@ -3,13 +3,20 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-       
+       discList: [],
       }
+    },
+    methods:{
+        getDiscList() {
+            axios.get('http://localhost/php-dischi-json/api/disc.php')
+        .then((response) =>{
+            this.discList = response.data;
+            console.log(response.data);
+         })
+        }
+    },
+    created(){
+        this.getDiscList();
     }
 
-
-
-
-
-    
   }).mount('#app')
